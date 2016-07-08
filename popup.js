fb0edbbce3fe604a54c163a9241ebb0cb0b3e479
avijeet <a@avijeets.com>
2016-07-07 23:18:17 -0400

    improved readability for popupjs

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.msg === 'trump') {
-    var dataQuery = {
-      active: true,
-      currentWindow: true
-    };
+    // only runs if trump msg is found
+    if (request.msg === 'trump') {
+        var dataQuery = {
+            active: true,
+            currentWindow: true
+        };
 
-    chrome.tabs.query(dataQuery, function(tabs) {
-      var dataUrl = tabs[0].url;
-      chrome.tabs.update({ url: 'transition.html'});
-      
-    });
-  }
-
+        //calls transition page after condition passes
+        chrome.tabs.query(dataQuery, function(tabs) {
+            var dataUrl = tabs[0].url; // grabs current url
+            chrome.tabs.update({  url: 'transition.html'   }); // moves to transition file
+        });
+    }
 });
